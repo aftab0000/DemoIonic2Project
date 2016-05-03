91c172087c57d23165559e3b64ee578ec4c70067
aftab0000 <aftab0000@gmail.com>
2016-05-04 01:17:02 +0600

    first commit

---
+++ app/pages/getting-started/getting-started.ts
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-angular';
+//import {Renderer, ElementRef, OnChanges} from 'angular2/core';
+
+@Page({
+  templateUrl: 'build/pages/getting-started/getting-started.html'
+})
+export class GettingStartedPage {
+  constructor() {
+    
+  }
+}
